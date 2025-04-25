51ccda070b2a6540172702dc38bad4bff4eaeb58
careybarberjr@gmail.com <careybarberjr@gmail.com>
2025-04-24 22:17:01 -0400

    a

--- BasicShapes.cpp
+++ BasicShapes.cpp
@@ -1,1 +1,33 @@
 #include "BasicShapes.h"
+using namespace std;
+
+
+circle::circle(double x, double y, double r, string n)
+{
+	setname();
+}
+
+double circle::calcArea()
+{
+	return 0.0;
+}
+
+rectangle::rectangle(double l, double w, string n)
+{
+
+}
+
+
+
+square::square(double s, string n)
+{
+}
+
+void BasicShapes::setname()
+{
+	name = n;
+}
+
+void BasicShapes::setarea()
+{
+}
--- BasicShapes.h
+++ BasicShapes.h
@@ -5,6 +5,50 @@ using namespace std;
 
 class BasicShapes
 {
-
+private:
+	double area;
+	string name;
+public:
+	
+	string getname() const { return name; };
+	double getarea() const { return area; };
+	void setname();
+	void setarea();
+	virtual double calcArea() = 0;
 };
 
+
+class circle : public BasicShapes {
+
+private:
+	double xcenter;
+	double ycenter;
+	double radius;
+public:
+	circle(double x, double y, double r, string n = "circle");
+
+	double getX() const { return xcenter; };
+	double getY() const { return ycenter; };
+	double getRadius() const { return radius; };
+	double calcArea() override;
+};
+
+class rectangle : BasicShapes {
+private:
+	double length;
+	double width;
+public:
+	rectangle(double l, double w, string n = "rectangle");
+
+		double getLength() const { return length; };
+		double getWidth() const { return width;  };
+};
+
+class square : public rectangle {
+private:
+	double side;
+public:
+	square(double s, string n = "square");
+	
+	double getSide() const { return side; };
+};
